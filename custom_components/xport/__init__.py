cc32d2b0446231ffa55f20915444d32561b858ab
Nils Bergmann <nilsbergmann@noim.io>
2023-05-12 16:23:11 +0200

    fix: hass 2023.3+

--- custom_components/xport/__init__.py
+++ custom_components/xport/__init__.py
@@ -19,7 +19,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = XPort(entry.data.get("host"))
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
