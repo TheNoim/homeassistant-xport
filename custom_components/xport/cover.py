408a6e80447aa6743cec4c168a4c40c3c19e9a06
Nils Bergmann <nilsbergmann@noim.io>
2025-01-18 14:14:41 +0100

    fix: covers

--- custom_components/xport/cover.py
+++ custom_components/xport/cover.py
@@ -1,8 +1,6 @@
 from homeassistant.components.cover import (
-    DEVICE_CLASS_BLIND,
-    SUPPORT_CLOSE,
-    SUPPORT_OPEN,
-    SUPPORT_STOP,
+    CoverDeviceClass,
+    CoverEntityFeature,
     CoverEntity,
 )
 from homeassistant.config_entries import ConfigEntry
@@ -11,17 +9,16 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 from .xport import XPort
 from .base_entity import BaseXportEntity
+import asyncio
 
 
 async def async_setup_entry(
     hass: HomeAssistant, config: ConfigEntry, async_add_entities, discovery_info=None
 ):
     """Set up platform."""
-    # Code for setting up your platform inside of the event loop.
-
     xport: XPort = hass.data[DOMAIN][config.entry_id]
 
-    covers: list[dict] = await hass.async_add_job(xport.covers)
+    covers: list[dict] = await asyncio.to_thread(xport.covers)
 
     entities = []
 
@@ -43,20 +40,19 @@ class XPortCover(CoverEntity, BaseXportEntity):
     @property
     def supported_features(self):
         if self._data.get("supportsHalf"):
-            return SUPPORT_STOP | SUPPORT_CLOSE | SUPPORT_OPEN
+            return CoverEntityFeature.STOP | CoverEntityFeature.CLOSE | CoverEntityFeature.OPEN
         else:
-            return SUPPORT_CLOSE | SUPPORT_OPEN
+            return CoverEntityFeature.CLOSE | CoverEntityFeature.OPEN
 
     @property
     def device_class(self):
-        return DEVICE_CLASS_BLIND
+        return CoverDeviceClass.BLIND
 
     @property
     def is_opening(self):
         if self._data.get("assumeInMotion") == True:
             if self._data.get("state") == "1" or self._data.get("state") == 1:
                 return True
-
         return False
 
     @property
@@ -64,7 +60,6 @@ class XPortCover(CoverEntity, BaseXportEntity):
         if self._data.get("assumeInMotion") == True:
             if self._data.get("state") == "2" or self._data.get("state") == 2:
                 return True
-
         return False
 
     @property
@@ -81,26 +76,22 @@ class XPortCover(CoverEntity, BaseXportEntity):
 
     async def async_update(self):
         """Update entity data"""
-
-        self._data = await self.hass.async_add_job(self._xport.get_cover, self._name)
+        self._data = await asyncio.to_thread(self._xport.get_cover, self._name)
 
     async def async_open_cover(self, **kwargs):
         """Open the cover."""
-
-        self._data = await self.hass.async_add_job(
+        self._data = await asyncio.to_thread(
             self._xport.do_cover_action, self._name, "UP"
         )
 
     async def async_close_cover(self, **kwargs):
         """Close cover."""
-
-        self._data = await self.hass.async_add_job(
+        self._data = await asyncio.to_thread(
             self._xport.do_cover_action, self._name, "DOWN"
         )
 
     async def async_stop_cover(self, **kwargs):
         """Stop the cover."""
-
-        self._data = await self.hass.async_add_job(
+        self._data = await asyncio.to_thread(
             self._xport.do_cover_action, self._name, "STOP"
         )
